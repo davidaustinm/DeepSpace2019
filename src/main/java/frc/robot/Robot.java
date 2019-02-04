ee429844c0495d72a9505d7e8691148c650e645a
Trisonics Driver Station <e3ghe3gh@gmail.com>
2019-02-04 18:52:03 -0500

    git testing2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
   public static TCPClient client = null;
 
   Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
    * This function is run when the robot is first started up and should be
